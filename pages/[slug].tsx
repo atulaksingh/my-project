aa539500138dbea146db9cbb360e468da169737b
atulaksingh <41638504+atulaksingh@users.noreply.github.com>
2023-07-02 23:34:04 +0530

    update

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,11 +1,10 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 const SlugPage: React.FC = () => {
-    const router = useRouter();
-    const { slug } = router.query;
-  
-    return <h1>Slug: {slug}</h1>;
-  };
-  
-  export default SlugPage;
-  
+  const router = useRouter();
+  const { slug } = router.query;
+
+  return <h1>Slufghbg: {slug}</h1>;
+};
+
+export default SlugPage;
